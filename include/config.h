89b7d4688ec6eaa0d1dae0c41f21e3604b8814fd
AEManov20 <aemanov20@codingburgas.bg>
2021-11-14 11:29:28 +0200

    Add build system

---
+++ include/config.h
@@ -0,0 +1,3 @@
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define WINDOW_TITLE "game"
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
+#include "config.h"
+
 #include <iostream>
+#include <string>
 #include <raylib.h>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText(std::to_string(GetFPS()).c_str(), 0, 0, 20, GREEN);
+            DrawText("Hello, World!", 100, 200, 20, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
